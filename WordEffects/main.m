f551c9a50184d0f458740afe76570aac92be89bd
will <will@willlam.net>
2016-01-11 18:30:46 -0500

    went through first 4 so far

--- WordEffects/main.m
+++ WordEffects/main.m
@@ -9,9 +9,36 @@
 #import <Foundation/Foundation.h>
 
 int main(int argc, const char * argv[]) {
-  @autoreleasepool {
-      // insert code here...
-      NSLog(@"Hello, World!");
+
+  char inputChars[255];
+  
+
+
+  printf("Input a string\n"); //getting a string input from user
+  fgets(inputChars, 255, stdin);
+  
+  
+// printf("Your string is %s\n", inputChars);
+ 
+ NSString *inputString = [NSString stringWithUTF8String:inputChars];
+  
+  
+  NSLog(@"Your input string was: %@", inputString);
+  
+  NSString *upperCase = [inputString uppercaseString];
+  NSLog(@"%@", upperCase);
+  
+  NSString *lowerCase = [inputString lowercaseString];
+  NSLog(@"%@", lowerCase);
+  
+  if ([inputString isMatchedByRegex:@"[^a-zA-Z0-9]"]) {
+    NSLog(@"This string contains illegal characters");
   }
+  
+  NSString *concatenate = [inputString stringByAppendingString:@", eh?"];
+   NSLog(@"%@", concatenate);
+ 
+  // NSlog(@"%"@, upper);
+  
     return 0;
 }
