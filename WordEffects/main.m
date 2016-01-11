236319a4b7e096b16c2ab44a3af298632f45e018
will <will@willlam.net>
2016-01-11 16:44:54 -0500

    Initial Commit

---
+++ WordEffects/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  WordEffects
+//
+//  Created by William Lam on 2016-01-11.
+//  Copyright © 2016 William Lam. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+  @autoreleasepool {
+      // insert code here...
+      NSLog(@"Hello, World!");
+  }
+    return 0;
+}
