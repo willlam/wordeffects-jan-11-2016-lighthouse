03aed4c958856803a78825e08ef83df0938302c9
will <will@willlam.net>
2016-01-11 19:16:50 -0500

    got some help and sanitized input string and display the presence of a number

--- WordEffects/main.m
+++ WordEffects/main.m
@@ -12,16 +12,18 @@ int main(int argc, const char * argv[]) {
 
   char inputChars[255];
   
+  
+  
+  NSNumberFormatter *numberFormatter = [[NSNumberFormatter alloc] init];
+  
+  /// variables
 
-
+  
   printf("Input a string\n"); //getting a string input from user
   fgets(inputChars, 255, stdin);
   
-  
-// printf("Your string is %s\n", inputChars);
- 
- NSString *inputString = [NSString stringWithUTF8String:inputChars];
-  
+  NSString *inputString = [NSString stringWithUTF8String:inputChars];
+
   
   NSLog(@"Your input string was: %@", inputString);
   
@@ -31,14 +33,24 @@ int main(int argc, const char * argv[]) {
   NSString *lowerCase = [inputString lowercaseString];
   NSLog(@"%@", lowerCase);
   
-  if ([inputString isMatchedByRegex:@"[^a-zA-Z0-9]"]) {
-    NSLog(@"This string contains illegal characters");
-  }
+  //Sanitize number string
   
+  NSCharacterSet *charactersToRemove = [[NSCharacterSet decimalDigitCharacterSet] invertedSet];
+  
+  NSString *sanitizedString = [[inputString componentsSeparatedByCharactersInSet:charactersToRemove] componentsJoinedByString:@""];
+  
+  NSNumber *presenceNumber = [numberFormatter numberFromString: sanitizedString];
+  
+  
+  
+  NSLog(@"The number in your string is %@", presenceNumber);
+//  if ([inputString isMatchedByRegex:@"[^a-zA-Z0-9]"]) {
+//    NSLog(@"This string contains illegal characters");
+//  }
+//  
   NSString *concatenate = [inputString stringByAppendingString:@", eh?"];
    NSLog(@"%@", concatenate);
  
-  // NSlog(@"%"@, upper);
   
     return 0;
 }
